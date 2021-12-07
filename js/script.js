68262a5bfe8a299e1687c2a08ad315384f9cf2c3
merzenniy <54005629+merzenniy@users.noreply.github.com>
2021-12-07 23:36:32 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.querySelector('.mobile_menu_trigger').addEventListener('click', () => document.querySelector('.nav_list').classList.toggle('show'));
+
+// let scrollHeight = document.body.scrollHeight;
+// let clientHeight = document.documentElement.clientHeight;
+
+// x = scrollHeight -clientHeight;
+
+// let navList = document.querySelector('.nav_list');
+
+// navList.style.height = `${x}px`;
